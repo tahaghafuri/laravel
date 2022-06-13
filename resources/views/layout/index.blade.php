3ef08bf623491f6c02346f4fc2e2b85e8e231ff9
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2022-06-13 17:02:10 +0430

    Initial commit

---
+++ resources/views/layout/index.blade.php
@@ -0,0 +1,3 @@
+@section('content')
+<b>test</b>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/contact', [TodoController::class,'contact']);
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/test', function (Illuminate\Http\Request $request) {
+
+    return $request->name;
+})->Middleware(['test','test1','test2']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
